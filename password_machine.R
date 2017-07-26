52b61e6e47ba17f2e7052b80418bffcc24769330
Jeremy Funk <jeremy.m.funk@gmail.com>
2017-07-26 14:03:20 -0400

    first commit

---
+++ password_machine.R
@@ -0,0 +1,26 @@
+
+password_machine <- function(len)  {
+  spcl <- c("!",  "#", "$", "%", "&", "(", ")", "*",  "+", "-", "/", ":",
+            ";", "<", "=", ">", "?", "@", "[", "^", "_", "{", "|", "}", "~")
+  
+  # start by choosing 1 of each char type, to ensure every password has coverage
+  s <- sample(spcl, 1)
+  l <- sample(letters, 1)
+  L <- sample(LETTERS, 1)
+  n <- sample(1:9, 1)
+  
+  # pool chars and assign probs for sampling
+  comb <- c(1:9, spcl, letters, LETTERS)
+  p <- c(rep(0.035, 9), rep(0.015, 25), rep(0.025, 52))
+  
+  # sample additional chars to complete 'len'
+  base <- c(s, l, L, n, sample(comb, len-4, TRUE, prob = p))
+  
+  # randomize char order and paste together
+  password <- paste0(sample(base), collapse = "")
+  
+  return(password)
+}
+
+p <- password_machine(10)
+p
